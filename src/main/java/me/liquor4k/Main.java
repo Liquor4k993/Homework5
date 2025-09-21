ae16380b30b181d3c26110856540c21d23c564ec
Софья <kiratuber01@gmail.com>
2025-09-21 20:42:46 +0300

    Задача 2

--- src/main/java/me/liquor4k/Main.java
+++ src/main/java/me/liquor4k/Main.java
@@ -3,12 +3,34 @@ package me.liquor4k;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задание 1");
+        //Определение ОС
         int clientOS = 0; // 0 - iOS, 1 - Android
 
         if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         } else if (clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Неизвестная операционная система");
+        }
+
+        System.out.println("\nЗадание 2");
+        //Определение ОС с учетом года
+        int clientOS2 = 1; // 0 - iOS, 1 - Android
+        int clientDeviceYear = 2015;
+
+        if (clientOS2 == 0) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        } else if (clientOS2 == 1) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
         } else {
             System.out.println("Неизвестная операционная система");
         }
