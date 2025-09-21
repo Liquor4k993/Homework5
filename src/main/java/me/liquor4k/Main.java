d5e43fd7e0df1eb01b025a539373ea01fa153f72
Софья <kiratuber01@gmail.com>
2025-09-21 20:38:31 +0300

    Задача 1

--- src/main/java/me/liquor4k/Main.java
+++ src/main/java/me/liquor4k/Main.java
@@ -1,2 +1,16 @@
 package me.liquor4k;
 
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задание 1");
+        int clientOS = 0; // 0 - iOS, 1 - Android
+
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Неизвестная операционная система");
+        }
+    }
+}
